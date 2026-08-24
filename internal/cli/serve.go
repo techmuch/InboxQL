6937a44c4e18ac584d5180456eab2db73075168b
David Fullmer <david.d.fullmer@gmail.com>
2026-08-24 19:39:02 -0400

    chore: bump version to 0.0.8

--- internal/cli/serve.go
+++ internal/cli/serve.go
@@ -14,7 +14,7 @@ import (
 // Version is the release version, overridable at build time with
 //
 //	go build -ldflags "-X github.com/user/uea/internal/cli.Version=1.2.3"
-var Version = "0.0.7"
+var Version = "0.0.8"
 
 func init() {
 	register(&Command{

0572433cd500b6a8516734f10e9878104227f4be
David Fullmer <david.d.fullmer@gmail.com>
2026-08-24 22:44:08 -0400

    chore: bump version to 0.0.20

--- internal/cli/serve.go
+++ internal/cli/serve.go
@@ -19,7 +19,7 @@ import (
 // Version is the release version, overridable at build time with
 //
 //	go build -ldflags "-X github.com/user/inboxql/internal/cli.Version=1.2.3"
-var Version = "0.0.19"
+var Version = "0.0.20"
 
 func init() {
 	register(&Command{

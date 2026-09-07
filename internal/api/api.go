efdae8e715b8836595a079d04c039531d634363f
David Fullmer <david.d.fullmer@gmail.com>
2026-09-07 16:47:54 -0400

    chore: bump version to 0.0.43

--- internal/api/api.go
+++ internal/api/api.go
@@ -188,7 +188,7 @@ type VersionInfo struct {
 }
 
 var currentVersionInfo = VersionInfo{
-	Version: "0.0.42",
+	Version: "0.0.43",
 }
 
 // SetVersionInfo sets the version metadata served at /api/version.
